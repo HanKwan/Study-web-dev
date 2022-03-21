d1153f7a62113d8a8283c74c3b20044620b5e197
Han Kwan <saihankwansaing@gmail.com>
2022-03-21 11:47:20 +0630

    restudy updating and if statement

--- app.js
+++ app.js
@@ -8,4 +8,42 @@ console.log(hello);
 function trueOrFalse(val){
     val === 10.4 ? 'True' : 'False';
 }
-console.log(trueOrFalse(10));
+console.log(trueOrFalse(10.34));
+
+function bigOrSmall (val){
+    if (val <= 10){
+        return 'Small';
+    } else if (val <= 10 && val >= 30){
+        return 'Medium';
+    } else {
+        return 'Large';
+    }
+}
+console.log(bigOrSmall(34));
+
+let coworkers = {
+    'coworker01': {
+        'name': 'john',
+        'nickname': ['jo'],
+        'income': 1000
+    },
+    'coworker02': {
+        'name': 'zero2',
+        'nickname': ['02'],
+        'income': 3000
+    }
+}
+function updating(id, prop, val){
+    if (val === ''){
+        delete coworkers[id][prop];
+    } else if (prop === 'nickname'){
+        coworkers[id][prop] = coworkers[id][prop] || [];
+        coworkers[id][prop].push(val);
+    } else {
+        coworkers[id][prop] = val;
+    }
+    return coworkers;
+}
+updating('coworker02', 'nickname', 'waifu');
+updating('coworker01', 'name', 'jomanji');
+console.log(coworkers);
