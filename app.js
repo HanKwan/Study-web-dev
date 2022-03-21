fd3ca122c2d27e70817afdeb9392a3e4f113d582
Han Kwan <saihankwansaing@gmail.com>
2022-03-21 09:44:36 +0630

    study intro

---
+++ app.js
@@ -0,0 +1,11 @@
+const firstName = 'Han';
+const middleName = 'Kwan';
+const lastName = 'Saing';
+let hello = (`Hello, my name is ${firstName} ${middleName} ${lastName}`);
+hello += ` and nice to meet u`;
+console.log(hello);
+
+function trueOrFalse(val){
+    val === 10.4 ? 'True' : 'False';
+}
+console.log(trueOrFalse(10));
