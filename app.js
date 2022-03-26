d415a19341881c5fec661759d95810a6dc36b71a
Han Kwan <saihankwansaing@gmail.com>
2022-03-26 10:12:11 +0630

    redo the pj again - now at adding btn

--- app.js
+++ app.js
@@ -1,46 +1,17 @@
-let form = document.querySelector('#form');
-let addH = document.querySelector('#addH');
-let submit = document.querySelector('#submit');
-let lists = document.querySelector('#lists');
-let searching = document.querySelector('#searching');
+let addH = document.querySelector('.addH');
+let addForm = document.querySelector('#addForm');
+let lists = document.querySelector('.lists');
 
-form.addEventListener('submit', added);
-lists.addEventListener('click', deleted);
-searching.addEventListener('keyup', searched);
+addForm.addEventListener('submit', submiting);
 
-function added(e) {
+function submiting(e) {
     e.preventDefault();
-    let newItem = addH.value;
+    let theItem = addH.value;
     let li = document.createElement('li');
-    li.className = 'aList';
-    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(theItem));
     lists.appendChild(li);
 
     addH.value = '';
 
-    let nBtn = document.createElement('button');
-    nBtn.className = 'xBtn';
-    nBtn.textContent = 'X';
-    li.appendChild(nBtn);
-}
-
-function deleted(e) {
-    if (e.target.classList.contains('xBtn')) {
-        let li = e.target.parentElement;
-        lists.removeChild(li);
-    }
-}
-
-function searched(e){
-    var text = e.target.value.toLowerCase();
-    var items = document.getElementsByTagName('li');
-    Array.from(items).forEach(function(z){
-        var names = z.firstChild.textContent;
-        if (names.toLowerCase().indexOf(text) != -1){
-            z.style.display = 'block';
-        } else {
-            z.style.display = 'none';
-        }
-    });
-    console.log(z);
+    
 }
