fffd9baa0282235a1a67414fb0f3b713d08dd659
Han Kwan <saihankwansaing@gmail.com>
2022-07-26 11:14:47 +0630

    make hover effect

--- testTailwind/app.js
+++ testTailwind/app.js
@@ -1,12 +1,8 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
+const menu2 = document.querySelector('#menu2');
 
-burger = addEventListener('click', dropDown);
-
-function dropDown() {
-    if (menu.classList.contain('hidden')) {
-        menu.classList.remove('hidden');        
-    } else {
-        menu.classList.add('hidden');
-    }
-}
+burger.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.toggle('hidden');
+});
